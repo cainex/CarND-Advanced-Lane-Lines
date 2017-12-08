ab831065dd200c18c1bbbdad86619223db0b6bb3
Daniel Grabowski <dmgrabowski@gmail.com>
2017-12-08 10:26:03 -0500

    more changes to lane image

--- lane_image.py
+++ lane_image.py
@@ -68,7 +68,12 @@ class lane_image:
         # Transformed image
         self.images['masked'] = self.mask_image(self.images['undistorted'])
         self.images['transform_grad'] = self.transform_image(self.mask_image(self.images['combined_grad']))
-        self.images['centroids'] = self.add_centroids_to_image(self.images['transform_grad'], self.find_window_centroids(self.images['transform_grad'], 50, 80, 100))
+        width = 50
+        height = 80
+        margin = 100
+        self.centroids = self.find_window_centroids(self.images['transform_grad'], width, height, margin)
+        print(self.centroids)
+        self.images['centroids'] = self.add_centroids_to_image(self.images['transform_grad'], self.centroids, width, height)
 
         
     def undistort_image(self):
@@ -220,7 +225,10 @@ class lane_image:
     def window_mask(self, width, height, img_ref, center,level):
         output = np.zeros_like(img_ref)
         output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height),max(0,int(center-width/2)):min(int(center+width/2),img_ref.shape[1])] = 1
-        return output
+
+        y_coord = np.uint32(((img_ref.shape[0]-level*height)+(img_ref.shape[0]-(level+1)*height))/2)
+
+        return output, [ np.uint32(center), y_coord]
 
     def find_window_centroids(self, image, window_width, window_height, margin):
         
@@ -259,7 +267,7 @@ class lane_image:
 
         return window_centroids
 
-    def add_centroids_to_image(self, img, window_centroids):
+    def add_centroids_to_image(self, img, window_centroids, width, height):
         # If we found any window centers
         if len(window_centroids) > 0:
 
@@ -267,11 +275,19 @@ class lane_image:
             l_points = np.zeros_like(img)
             r_points = np.zeros_like(img)
 
+            l_center_points = []
+            r_center_points = []
+
             # Go through each level and draw the windows 	
             for level in range(0,len(window_centroids)):
                 # Window_mask is a function to draw window areas
-                l_mask = self.window_mask(50,80,img,window_centroids[level][0],level)
-                r_mask = self.window_mask(50,80,img,window_centroids[level][1],level)
+                l_mask, l_center = self.window_mask(width,height,img,window_centroids[level][0],level)
+                r_mask, r_center = self.window_mask(width,height,img,window_centroids[level][1],level)
+
+                print(l_center)
+                l_center_points.append(l_center)
+                r_center_points.append(r_center)
+
                 # Add graphic points from window mask here to total pixels found 
                 l_points[(l_points == 255) | ((l_mask == 1) ) ] = 255
                 r_points[(r_points == 255) | ((r_mask == 1) ) ] = 255
@@ -282,7 +298,27 @@ class lane_image:
             template = np.array(cv2.merge((zero_channel,template,zero_channel)),np.uint8) # make window pixels green
             warpage= np.dstack((img, img, img))*255 # making the original road pixels 3 color channels
             output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0) # overlay the orignal road image with window results
+
+            for center_point in l_center_points:
+                cv2.circle(output, (center_point[0], center_point[1]), 5, (128,128,255), -1)
+
+            for center_point in r_center_points:
+                cv2.circle(output, (center_point[0], center_point[1]), 5, (255,128,128), -1)
         
+            left_fit = np.polyfit(np.array(l_center_points)[:,0], np.array(l_center_points)[:,1], 2)
+            right_fit = np.polyfit(np.array(r_center_points)[:,0], np.array(r_center_points)[:,1], 2)
+
+            ploty = np.linspace(0, output.shape[0]-1, output.shape[0] )
+            left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+            right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+            fig, ax = plt.subplots(ncols=1, nrows=1)
+            ax.imshow(output)
+            ax.plot(left_fitx, ploty, color='yellow')
+            ax.plot(right_fitx, ploty, color='yellow')
+
+        #    plt.show()
+
         # If no window centers found, just display orginal road image
         else:
             output = np.array(cv2.merge((img,img,img)),np.uint8)
